1eb89037424bf449cfcf1c13838c00fad8c289b1
Mauricio Soares <m.soares2603@gmail.com>
2016-03-10 20:40:03 -0300

    move back from bin to root folder.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
 const webpack = require('webpack');
-const getWebpackConfig = require('../lib/webpack');
+const getWebpackConfig = require('./lib/webpack');
+var ProgressPlugin = require('webpack/lib/ProgressPlugin');
+
 
 var compiler = webpack(getWebpackConfig());
 
 compiler.watch({
   aggregateTimeout: 300,
-  poll: true
+  poll: true,
+  errorDetails: true
 }, function(err, stats) {
   if(err) {
     console.log('error!');
     return;
   }
 
-  console.log(stats);
   console.log(stats.hasErrors());
   console.log(':D');
 });
